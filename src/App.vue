84d25fddf574ba8a3917c0c245978dad7e1111c2
Raul_Guichard <raulguichardv@gmail.com>
2025-10-06 22:59:45 -0600

    se creo la primera version del menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
-<script setup></script>
+<script setup>
+import MenuBar from './components/MenuBar.vue';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <header class="container_menu">
+      <MenuBar/>
+    </header>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<template>
+    <menu class="menu">
+        <RouterLink class="views active" to="/">Home</RouterLink>
+        <RouterLink class="views" to="/ifelse">If-else</RouterLink>
+        <RouterLink class="views" to="/show">Show</RouterLink>
+        <RouterLink class="views" to="/for">For</RouterLink>
+        <RouterLink class="views" to="/bind">Bind</RouterLink>
+    </menu>
+</template>
+
+<style>
+    .menu{
+        width: 100%;
+        height: 100px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+    .views{
+        text-decoration: none;
+        color:#000;
+        border-bottom: 1px solid transparent;
+        transition: all 0.5s ease;
+    }
+    .views:hover{
+        border-bottom: 1px solid #000;
+    }
+    .active{
+        border-bottom: 1px solid #000;
+    }
+</style>
